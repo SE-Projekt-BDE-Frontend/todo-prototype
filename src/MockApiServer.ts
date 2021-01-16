151a4d62f62e3f21afa9ce6591bf8fc643dc37b2
Julian Meinking <s80448@htw-dresden.de>
2021-01-17 00:03:49 +0100

    feat(mock): setup local mock api with miragejs

---
+++ src/MockApiServer.ts
@@ -0,0 +1,39 @@
+import { createServer, Model, RestSerializer } from "miragejs"
+
+const mockModels = {
+  todo: Model.extend<Partial<{ title: string; done: boolean }>>({}),
+}
+
+export function makeServer({ environment = "development" } = {}) {
+  const server = createServer<typeof mockModels, {}>({
+    environment,
+    serializers: {
+      application: RestSerializer,
+    },
+
+    models: {
+      todo: Model,
+    },
+
+    seeds(server) {
+      server.create("todo", { title: "Buy Fruits", done: false })
+      server.create("todo", { title: "Homework", done: true })
+      server.create("todo", {
+        title: "Learn to write efficient JavaScript",
+        done: false,
+      })
+    },
+
+    routes() {
+      this.namespace = "api"
+
+      this.get("/todos")
+      this.get("/todos/:id")
+      this.post("/todos")
+      this.patch("/todos/:id")
+      this.del("/todos/:id")
+    },
+  })
+
+  return server
+}
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import "./registerServiceWorker"
 import router from "./router"
+import { makeServer } from "./MockApiServer"
+
+if (process.env.NODE_ENV === "development") {
+  makeServer()
+}
 
 createApp(App).use(router).mount("#app")
